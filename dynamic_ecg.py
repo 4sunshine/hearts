ff83e946b4b6b145e19cbe7819854cd7b5234be4
MaximovaIrina <irina.maximovva@gmail.com>
2021-06-13 10:10:26 +0300

    add dynamic_ecg file

---
+++ dynamic_ecg.py
@@ -0,0 +1,44 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import argparse
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Dynamic ECG')
+    parser.add_argument('-id', action='store', help='People ID', default=1)
+    # # parser.add_argument('-speed', action='store', help='Speed', default=1)
+    # parser.add_argument('-start', action='store', help='start', default=0)
+    # parser.add_argument('-end', action='store', help='end', default=1e9)
+    args = parser.parse_args()
+
+    df = pd.read_csv('train.csv')
+    person = df.loc[df['id'] == args.id]
+    person['time'] = person['time'] / 60000
+
+    time, RR, label = person['time'].to_numpy(), person['x'].to_numpy(), person['y'].to_numpy()
+    delta = np.asarray([time[i] - time[i-1] for i in range(1, len(time))])
+    mean_delta, std_delta = delta.mean(), delta.std()
+
+    plt.rcParams['animation.html'] = 'jshtml'
+    fig = plt.figure(figsize=(15, 5))
+    ax = fig.add_subplot(111)
+    plt.xlabel("Время, мин")
+    plt.ylabel("R-R интервал")
+    plt.title(f"Real-time ритмограмма пациента N{1}")
+    fig.show()
+
+    n_pionts_min = int(len(time) / max(time))
+    n_min_show = 3
+    n_points_show = n_min_show * n_pionts_min
+
+    time = np.concatenate([-1 * n_min_show * np.arange(0, 1, 1 / n_points_show)[::-1], time])
+    RR = np.concatenate([np.full(n_points_show, RR.mean()), RR])
+    label = np.concatenate([np.zeros(n_points_show), label])
+    plt.plot(time, RR)
+    [plt.fill_between([time[i], ], 0, [RR[i], ], color='red', alpha=0.2) for i in range(len(time)) if label[i] == 1]
+
+    for i in range(n_points_show, len(time)):
+        plt.xlim(left=time[i - n_points_show], right=time[i])
+        print()
+        fig.canvas.draw()
+        fig.canvas.flush_events()
