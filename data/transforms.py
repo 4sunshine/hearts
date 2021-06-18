c7bd4ee4c6206ac3dbcd7fe8822d47fd519568ca
Shain Alexander <shain.alexander@huawei.com>
2021-06-19 01:19:16 +0300

    sigma crop + ecg upd

--- data/transforms.py
+++ data/transforms.py
@@ -60,6 +60,17 @@ class PadZeros(object):
         return sample
 
 
+class SigmaCrop(object):
+
+    def __call__(self, sample):
+        person, labels, mask = sample['person'], sample['labels'], sample['mask']
+        mean, std = person.mean(), person.std()
+        person[1][person[1] > mean + std] = mean + std
+        person[1][person[1] < mean - std] = mean - std
+        sample['person'] = person
+        return sample
+
+
 class ToTensor(object):
     """Convert ndarrays in sample to Tensors."""
     def __call__(self, sample):
@@ -92,6 +103,7 @@ def get_base_transform(cfg):
 def get_sequence_transform(cfg):
     base_transform = transforms.Compose([
         Normalize((cfg.RR_MEAN, cfg.RR_STD), (0, cfg.RR_MEAN * cfg.MAX_N_TICKS / 2.)),
+        SigmaCrop(),
         ToSequenceTensor()
     ])
     return base_transform
--- dynamic_ecg.py
+++ dynamic_ecg.py
@@ -1,7 +1,6 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
-import argparse
 
 
 class FigPlotter:
@@ -13,9 +12,14 @@ class FigPlotter:
         prediction = prediction[RR != 0]
         time = time[RR != 0]
 
+        mean, std = RR.mean(), RR.std()
+        # RR[RR > mean + std] = mean + std
+        # RR[RR < mean - std] = mean - std
+
         plt.plot(time, RR)
         [plt.fill_between([time[i], ], [min(RR), ], [RR[i], ], color='red', alpha=0.2) for i in range(len(time)) if target[i] == 1]
         [plt.fill_between([time[i], ], [RR[i], ], [max(RR), ], color='green', alpha=0.2) for i in range(len(time)) if prediction[i] == 1]
+        [plt.fill_between([time[i], ], [min(RR), ], [max(RR), ], color='green', alpha=0.2) for i in range(len(time)) if RR[i] > (mean + 2 * std)]
         plt.xlabel("Время, мин")
         plt.ylabel("R-R интервал")
         plt.title(f"Ритмограмма пациента N{person_id}")
@@ -73,7 +77,6 @@ class FigPlotter:
         plt.tight_layout()
         plt.savefig(f'covid {id}')
 
-
     @staticmethod
     def show():
         plt.show()
@@ -90,16 +93,17 @@ class FigPlotter:
 if __name__ == '__main__':
     df = pd.read_csv('data/train.csv')
 
-    for id in set(df['id']):
+    for id in [4, ]:
+    # for id in set(df['id']):
         person = df.loc[df['id'] == id]
         time, RR, label = person['time'].to_numpy(), person['x'].to_numpy(), person['y'].to_numpy()
-        # FigPlotter.plot_ecg(time, RR, label, np.zeros(len(RR)), id)
+        FigPlotter.plot_ecg(time, RR, label, np.zeros(len(RR)), id)
+        FigPlotter.show()
+        FigPlotter.refresh()
+        # FigPlotter.plot_covid(time, RR, label, id)
         # FigPlotter.show()
         # FigPlotter.refresh()
-        FigPlotter.plot_covid(time, RR, label, id)
-        # FigPlotter.show()
-        FigPlotter.refresh()
-        print(id)
+        # print(id)
 
     # parser = argparse.ArgumentParser(description='Dynamic ECG')
     # parser.add_argument('-id', action='store', help='People ID', default=1)
